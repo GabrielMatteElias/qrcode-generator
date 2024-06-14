178916f71fd3e6780bd85225e4e467f7b44ab694
gabrielmatteelias <gabrielmatteelias@gmail.com>
2024-06-14 17:09:56 -0300

    Adjusted responsiveness and empty input validation

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const img = document.getElementById('img');
 
 async function generateQrcode() {
     const inputValue = input.value
+    if(!inputValue) return
     const qr = `https://api.qrserver.com/v1/create-qr-code/?color=52734D&bgcolor=DDFFBC&size=500X500&data=${inputValue}`
     img.src = qr;
 }
